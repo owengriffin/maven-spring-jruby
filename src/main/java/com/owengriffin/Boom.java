915cb5676f63ea401d805a9c4019efbd2c68e3bb
Owen Griffin <owen.griffin@vodafone.com>
2010-10-29 08:29:53 +0100

    Asserting boom in test

--- src/main/java/com/owengriffin/Boom.java
+++ src/main/java/com/owengriffin/Boom.java
@@ -1,10 +1,21 @@
 package com.owengriffin;
 
 import org.jruby.Ruby;
+import org.jruby.embed.ScriptingContainer;
 import org.jruby.internal.runtime.GlobalVariables;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class Boom {
+	
+	@Autowired
+	private Ruby scriptingContainer;
+	
+	public Ruby getScriptingContainer() {
+		return this.scriptingContainer;
+	}
+	
 public Boom() {
+	
 	String home = "/home/ogriffin/.rvm/rubies/jruby-1.5.2/";
 	System.setProperty("jruby.home", home);
 	System.setProperty("jruby.gem.home", "/home/ogriffin/.rvm/gems/jruby-1.5.2/gems");
